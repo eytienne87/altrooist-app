b359747fd8e1152e05a416bf11eb53db221e4456
VanThao Huynh <huynhcasi@gmail.com>
2021-08-24 15:28:23 -0400

    change-description-data-type-in-organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_182614) do
+ActiveRecord::Schema.define(version: 2021_08_24_192630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_182614) do
 
   create_table "organizations", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.string "address"
     t.float "latitude"
     t.float "longitude"
