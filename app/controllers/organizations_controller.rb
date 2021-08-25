ca817db22af115693df445fdbb18d8ee89531df5
VanThao Huynh <huynhcasi@gmail.com>
2021-08-25 16:46:09 -0400

    create org show page

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,16 @@
 class OrganizationsController < ApplicationController
+  before_action :set_org, only: [:show]
+
   def curated
     @curated = Organization.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_org
+    @org = Organization.find(params[:id])
   end
 end
