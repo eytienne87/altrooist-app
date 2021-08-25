5637e12ea886b42e6fcd08855b0602dd962cb671
Etienne Tessier <etienne_tessier100@hotmail.ca>
2021-08-24 21:44:06 -0400

    installed simple form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  accepts_nested_attributes_for :location, :interest
+  # accepts_nested_attributes_for :location, :interest
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'profile/:id/edit', to: 'profiles#edit'
+  get 'profile/edit', to: 'profiles#edit'
   patch 'profile', to: 'profiles#update'
 end
