1b5d3eaf11f61848c75b9d2b2ecc13e3ac9f7fe4
VanThao Huynh <huynhcasi@gmail.com>
2021-08-25 11:38:54 -0400

    create organization routes, controller and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Organization.destroy_all
+Category.destroy_all
+
+puts "creating category seed...."
+cat1 = Category.new({ name: "animal" })
+cat1.save!
+puts "done"
+
+puts "now creating organization"
+name = 'SPCA'
+description = "Founded in Montreal in 1869, the Society for the Prevention of Cruelty to Animals (now known simply as the Montreal SPCA) was the first animal-welfare organization in Canada."
+address = "5215 Rue Jean-Talon O, Montréal, QC H4P 1X4"
+latitude = 45.495960
+longitude = -73.652206
+category_id = cat1.id
+org1 = Organization.create({ name: name, description: description, address: address, latitude: latitude, longitude: longitude, category_id: category_id })
+org1.save!
+puts "finished"
