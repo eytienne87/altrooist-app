917076e6f1f41b0983d722fbf884a7c929b91c89
VanThao Huynh <huynhcasi@gmail.com>
2021-08-25 20:29:29 -0400

    add email phone and photo to organizations

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,8 +101,8 @@ interest5 = Interest.create!(
 Organization.create!(
   name: 'SPCA',
   email: 'cs@spca.ca',
-  phone: '15148240013',
-  photo_url: '/app/assets/images/spca.jpg',
+  phone: '(514) 824-0013',
+  photo_url: 'spca.jpg',
   description: "Founded in Montreal in 1869, the Society for the Prevention of Cruelty to Animals (now known simply as the Montreal SPCA) was the first animal-welfare organization in Canada.",
   address: "5215 Rue Jean-Talon O, Montréal, QC H4P 1X4",
   latitude: 45.495960,
@@ -111,6 +111,9 @@ Organization.create!(
 )
 Organization.create!(
   name: "Entraide Bénévole Métro (EBM)",
+  email: 'cs@ebm.ca',
+  phone: '(514) 721-5594',
+  photo_url: 'ebm.png',
   description: "We are committed to providing physical and emotional support to seniors with temporary or permanent loss of autonomy. Our involvement assists clients in breaking their social isolation, maintaining their autonomy, and remaining in the community.",
   address: "1801 de Maisonneuve West,Montreal, Quebec H3H 1J9",
   latitude: 45.463850,
@@ -119,6 +122,9 @@ Organization.create!(
 )
 Organization.create!(
   name: 'AQVA - Association Quebecoise De Voile Adaptee',
+  email: 'cs@aqva.com',
+  phone: '(514) 714-8825',
+  photo_url: 'aqva.png',
   description: "Our mission is to enable people with mobility impairments, including very severe ones, to increase the quality of their life and to reintegrate in the community by means of sailing.",
   address: "2025 Rue Quesnel, Montréal, QC H3J 2K9",
   latitude: 45.488220,
@@ -127,6 +133,9 @@ Organization.create!(
 )
 Organization.create!(
   name: 'Benedict Labre House',
+  email: 'cs@labrehouse.ca',
+  phone: '(514) 864-7725',
+  photo_url: 'lamaison.jpg',
   description: "The Benedict Labre House is a home that nourishes the body, mind and soul of people in need. It is a volunteer driven charity deeply rooted in the Montreal community since 1952. The House nurtures and empowers our guests through support programs based on the principles of respect and a sense of belonging.",
   address: "308 Rue Young, Montréal, QC H3C 2G2",
   latitude: 45.493900,
@@ -135,9 +144,13 @@ Organization.create!(
 )
 Organization.create!(
   name: "Mentorat à l'école",
+  email: 'cs@mentorat.ca',
+  phone: '(514) 549-8853',
+  photo_url: 'mentorat.png',
   description: "Le programme de Mentorat à l’école propose aux filles et aux garçons de rencontrer une personne qui sert de modèle, un.eami.e à qui ils.elles peuvent parler et partager leurs expériences de vie.",
   address: "3155 Rue Hochelaga, Montréal, QC H1W 1G4",
   latitude: 45.542439,
   longitude: -73.554428,
   category: education
 )
+puts "Finished"
