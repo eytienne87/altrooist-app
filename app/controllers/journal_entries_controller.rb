a3f75404c09b9bb67b60a21b171beecc59f948be
VanThao Huynh <huynhcasi@gmail.com>
2021-08-31 15:23:14 -0400

    add new emoji

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -17,15 +17,14 @@ class JournalEntriesController < ApplicationController
     if current_user.organizations.include?(@organization)
       @journal_entry = JournalEntry.new(journal_entry_params)
     else
-      @journal_entry = JournalEntry.new(content: "I have just volunterring this organization", emoji: "&#128512;")
+      @journal_entry = JournalEntry.new(content: "I have just volunterring this organization", emoji: "fas fa-hands-helping")
     end
 
     @journal_entry.organization = @organization
     @journal_entry.user = current_user
 
     if @journal_entry.save
-      # redirect_to new_volunteering_page
-      redirect_to my_organizations_path
+      redirect_to organization_journal_entries_path
     else
       render :new
     end
