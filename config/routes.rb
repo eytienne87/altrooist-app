3859bc2b278db67aeda88f77e364654df0229e94
Stephane Lafontaine <stephane@medqigong.ca>
2021-09-01 10:36:06 -0400

    Created profile show page; added show view, profiles_controller#show; added releveant route; pointed link in navbar avatar to relevant route; build html structure as per Figma

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :profile, only: [:update, :edit]
+  resource :profile, only: [:show, :update, :edit]
 
   get '/organizations/curated', to: 'organizations#curated', as: :curated
   get '/organizations/:id', to: 'organizations#show', as: :organization
